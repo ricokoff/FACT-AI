a56b0cb4e6448dae04c80c8154ec6a3ad35dac30
ykerus <yke.rusticus@gmail.com>
2020-01-28 11:56:09 +0100

    add function for faithfulness plot

--- SENN/api/mnist.py
+++ SENN/api/mnist.py
@@ -498,3 +498,52 @@ def evaluate(model, dataset, print_freq=1000, return_acc=False):
     if return_acc:
         return acc
     return
+
+def faithfullness_plot(model, dataset, indx, show_h=False, show_htheta=True):
+    #sometimes the model is so certain of a given class that removing a theta_i has no effect
+    prob_drop = []
+    x, t = mnist.get_digit(dataset, indx)
+    nconcepts = model.parametrizer.nconcept
+    with torch.no_grad():
+        theta_x = model.parametrizer(x.view(1,1,28,28))
+        h_x = model.conceptizer.encode(x.view(1,1,28,28))
+        probs_0 = torch.softmax(model(x.view(1,1,28,28)).squeeze(),dim=0)
+        prob_t = probs_0[t]
+        for i in range(nconcepts):
+            theta_i = torch.tensor(theta_x)
+            theta_i[0,i,:]=0.
+            prob_i = torch.softmax(model.aggregator(h_x, theta_i).squeeze(),dim=0)[t]
+            prob_drop.append(prob_t - prob_i)
+    plt.title("Faithfulness plot for a single sample")
+    plt.xlabel("Concept Index")
+    
+    ax1 = plt.subplot()
+    ax1.bar(range(len(theta_x.squeeze()[:,t])), theta_x.squeeze()[:,t])
+    plt.ylabel(r"Feature Relevance $\theta(x)_i$")
+    ax2 = ax1.twinx()
+    ax2.plot(range(nconcepts), prob_drop, "--", color="orange")
+    ax2.scatter(range(nconcepts), [float(i) for i in prob_drop], color="orange")
+    plt.ticklabel_format(style="scientific",axis="y",scilimits=(0,0))
+    plt.ylabel(r"Probability Drop") 
+    plt.xticks(range(nconcepts),[str(i) for i in range(1,nconcepts+1)])   
+    plt.tight_layout()
+    plt.show()
+    
+    h_x = h_x.squeeze()
+    theta_x = theta_x.squeeze()
+    if show_h:
+        plt.title("Concept activation")
+        plt.xlabel("Concept Index")
+        plt.ylabel(r"$h(x)_i$")
+        plt.bar(range(nconcepts),h_x)
+        plt.xticks(range(nconcepts),[str(i) for i in range(1,nconcepts+1)])
+        plt.show()
+    
+    if show_htheta:
+        plt.title("Relevance multiplied by concept activation")
+        plt.xlabel("Concept Index")
+        plt.ylabel(r"$\theta(x)_i \cdot h(x)_i$")
+        plt.bar(range(nconcepts),np.array(h_x)*np.array(theta_x[:,t]))
+        plt.xticks(range(nconcepts),[str(i) for i in range(1,nconcepts+1)])
+        plt.show()
+    return
